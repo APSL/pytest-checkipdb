782cbcd128d39d3a8eb981bfd5b080d11528da26
Andreu Vallbona <avallbona@gmail.com>
2017-02-26 12:47:26 +0100

    some refactor

---
+++ test_checkipdb.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+pytest_plugins = 'pytester',
+
+
+@pytest.fixture
+def example(request):
+    """Create a test file."""
+    testdir = request.getfuncargvalue("testdir")
+    p = testdir.makepyfile("")
+    p.write("class AClass:\n\tpass\n\t\t\n\n# too many spaces")
+    return p
+
+
+# @pytest.fixture
+# def example_ko(request):
+#     """Create a test file."""
+#     testdir = request.getfuncargvalue("testdir")
+#     p = testdir.makepyfile("")
+#     p.write("import ipdb; ipdb.set_trace()\nclass AClass:\n\tpass\n\t\t\n\n# too many spaces")
+#     return p
+
+
+def test_ok(testdir, example, request):
+    result = testdir.runpytest()
+    result.stdout.fnmatch_lines([
+        '*PASSED*',
+    ])
+    assert result.ret != 0
+
+
+# def test_ko(testdir, example_ko):
+#     result = testdir.runpytest()
+#     result.stdout.fnmatch_lines([
+#         '*PASSED*',
+#     ])
+#     assert result.ret != 0
