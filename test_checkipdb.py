664e0dae5a4a56fd29b2e2edd040e3ab7941b6d9
Andreu Vallbona <avallbona@gmail.com>
2017-02-26 12:51:19 +0100

    messing up with tests

--- test_checkipdb.py
+++ test_checkipdb.py
@@ -14,26 +14,26 @@ def example(request):
     return p
 
 
-# @pytest.fixture
-# def example_ko(request):
-#     """Create a test file."""
-#     testdir = request.getfuncargvalue("testdir")
-#     p = testdir.makepyfile("")
-#     p.write("import ipdb; ipdb.set_trace()\nclass AClass:\n\tpass\n\t\t\n\n# too many spaces")
-#     return p
+@pytest.fixture
+def example_ko(request):
+    """Create a test file."""
+    testdir = request.getfuncargvalue("testdir")
+    p = testdir.makepyfile("")
+    p.write("import ipdb; ipdb.set_trace()\nclass AClass:\n\tpass\n\t\t\n\n# too many spaces")
+    return p
 
 
 def test_ok(testdir, example, request):
     result = testdir.runpytest()
-    result.stdout.fnmatch_lines([
-        '*PASSED*',
-    ])
+    # result.stdout.fnmatch_lines([
+    #     '*PASSED*',
+    # ])
     assert result.ret != 0
 
 
-# def test_ko(testdir, example_ko):
-#     result = testdir.runpytest()
-#     result.stdout.fnmatch_lines([
-#         '*PASSED*',
-#     ])
-#     assert result.ret != 0
+def test_ko(testdir, example_ko):
+    result = testdir.runpytest()
+    # result.stdout.fnmatch_lines([
+    #     '*PASSED*',
+    # ])
+    assert result.ret != 0
