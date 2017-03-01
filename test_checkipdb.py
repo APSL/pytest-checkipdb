11c8f4c0044c6ad910731bacb3e2851f7b084f27
Andreu <avallbona@gmail.com>
2017-03-01 02:39:05 +0100

    fixing tests

--- test_checkipdb.py
+++ test_checkipdb.py
@@ -1,62 +1,31 @@
 # -*- coding: utf-8 -*-
 
-import pytest
-
-pytest_plugins = "pytester"
+pytest_plugins = "pytester",
 
 
-# @pytest.fixture
-# def example_ok(request):
-#     """Create a test file."""
-#     testdir = request.getfuncargvalue("testdir")
-#     p = testdir.makepyfile("")
-#     p.write("class AClass:\n\tpass\n\t\t\n\n# too many spaces")
-#     return p
-#
-#
-# @pytest.fixture
-# def example_ko(request):
-#     """Create a test file."""
-#     testdir = request.getfuncargvalue("testdir")
-#     p = testdir.makepyfile("")
-#     p.write("import ipdb; ipdb.set_trace()\nclass AClass:\n\tpass\n\t\t\n\n# too many spaces")
-#     return p
-#
-#
-# def test_ok(testdir, example_ok, request):
-#     testdir.tmpdir.ensure("test_ok.py")
-#     result = testdir.runpytest("--cipdb --verbose", )
-#     result.stdout.fnmatch_lines([
-#         '*no tests*',
-#     ])
-#     assert result.ret != 0
-#
-#
-# def test_ko(testdir, example_ko):
-#     testdir.tmpdir.ensure("test_ko.py")
-#     result = testdir.runpytest("--cipdb --verbose", )
-#     result.stdout.fnmatch_lines([
-#         '*no tests*',
-#     ])
-#     assert result.ret != 0
+class TestBasic(object):
 
+    def test_ok(self, testdir):
+        testdir.makepyfile("""
+            def test_ok():
+                pass
+        """)
+        result = testdir.runpytest()
+        result.stdout.fnmatch_lines('test_ok.py .')
 
-def test_myplugin(testdir):
-    testdir.makepyfile("""
-        def test_example():
-            pass
-    """)
-    result = testdir.runpytest("--verbose")
-    result.stdout.fnmatch_lines('*PASSED*')
+    def test_ko(self, testdir):
+        testdir.makepyfile("""
+            # monkeypatching ipdb
+            def foo():
+                return True
 
-
-def test_ok(testdir):
-    testdir.makepyfile("""
-        def test_ok():
-            import ipdb; ipdb.set_trace()
-            pass
-    """)
-    result = testdir.runpytest("--verbose")
-    result.stdout.fnmatch_lines('*PASSED*')
+            def test_ko():
+                import ipdb;
+                ipdb.set_trace = foo
+                ipdb.set_trace()
+                pass
+        """)
+        result = testdir.runpytest()
+        result.stdout.fnmatch_lines('test_ko.py .')
 
 
